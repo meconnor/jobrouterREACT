65d585f4acf48d5376db7daa534b709dabe5e8e7
Connor <c.meriweather55@gmail.com>
2023-11-30 11:47:18 -0600

    added careers page

--- src/App.js
+++ src/App.js
@@ -9,20 +9,28 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Faq from "./pages/help/Faq";
 import Contact from "./pages/help/Contact";
+import Careers from "./pages/careers/Careers";
 //layouts
 import RootLayout from "./layouts/RootLayout";
 import HelpLayout from "./layouts/HelpLayout";
 import NotFound from "./pages/NotFound";
+import CareersLayout from "./layouts/CareersLayout";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
+
       <Route path="help" element={<HelpLayout />}>
         <Route path="faq" element={<Faq />} />
         <Route path="contact" element={<Contact />} />
       </Route>
+
+      <Route path="carrers" element={<CareersLayout />}>
+        <Route index element={<Careers />} />
+      </Route>
+
       <Route path="*" element={<NotFound />} />
     </Route>
   )
---
+++ src/pages/careers/Careers.js
@@ -0,0 +1,7 @@
+export default function Careers() {
+  return <div className="careers"></div>;
+}
+
+//loader function
+
+const careersLoader = async () => {};
